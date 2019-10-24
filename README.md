3a7c2b3ca702cb046ddbc1fb84e56adc296acba2
prof-csh <prof.csh@gmail.com>
2019-10-23 22:03:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio-csh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/portfolio-csh)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+// import { Material } from '@angular/material/card';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent }
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
