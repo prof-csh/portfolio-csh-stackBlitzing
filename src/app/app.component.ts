d19864561fa36e42ae3b8807962fa70f1387cfd0
prof-csh <prof.csh@gmail.com>
2019-11-07 12:55:44 -0800

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { hello } from '../scripts/custom.js';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  ngOnInit()  {
+      hello()
+
+  }
 }
